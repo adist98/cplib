8ad16cb05f0eda3c15192bf4392d0c62ef899b52
Aditya Soni <adist98@gmail.com>
2019-10-25 21:23:36 +0530

    Update binarySearchUsual.cpp

--- binarySearchUsual.cpp
+++ binarySearchUsual.cpp
@@ -1,16 +1,21 @@
-#The usual Binary Search algorithm
-
+// The usual Binary Search algorithm
+// Find the target element in a given sequence
 int main()
 {
   int l,r;
   l = 0;
   r = n-1;
+  // n is the size of sequence
   int mid = -1;
+  int number;
+  cin >> number;
   int target = number; // we have to find this number in a given sequence using binarysearch
+  int flag = 0;
   while(l <= r){
     mid = l + (r-l)/2; // this is to avoid overflow
     if(a[mid] == target){
       cout << mid;
+      flag = 1;
       break;
     }
     if(a[mid] < target){
@@ -19,3 +24,7 @@ int main()
       r = mid - 1;
     }
   }
+  if(flag == 0){
+    cout << "Element not found" << endl;
+  }
+}
